e96ccb74fcbbb0704446843b6bed8f81e0942d1e
KishiTheMechanic <s.kishi@elsoul.nl>
2023-03-13 12:53:02 +0100

    blog layout

--- src/components/articles/blog/BlogContents.tsx
+++ src/components/articles/blog/BlogContents.tsx
@@ -12,30 +12,30 @@ export default function BlogContents({ article, articleHtml }: Props) {
   return (
     <>
       <Container>
-        <div className="flex justify-center py-12 lg:gap-12">
-          <div>
+        <div className="flex justify-center py-12 lg:grid lg:grid-cols-8 lg:gap-12">
+          <div className="lg:col-span-5">
             <h1 className="text-4xl font-bold">{article.title}</h1>
             <p className="mt-1 text-gray-600 dark:text-gray-200">
               {article.date}
             </p>
-            <div className="py-8">
-              <Image
-                src={article.thumbnail}
-                alt={article.title}
-                width="16"
-                height="9"
-                className="aspect-[16/9] w-full bg-gray-100 object-cover group-hover:opacity-80 sm:aspect-[2/1] lg:aspect-[3/2]"
-                unoptimized
-              />
-            </div>
-            <div className="py-8 lg:hidden">
-              <ScrollSyncToc rawMarkdownBody={article.content} />
-            </div>
-            <div className="prose dark:prose-invert  lg:prose-lg">
+            <div className="prose dark:prose-invert lg:prose-lg">
+              <div className="py-8">
+                <Image
+                  src={article.thumbnail}
+                  alt={article.title}
+                  width="16"
+                  height="9"
+                  className="aspect-video w-full bg-gray-100 object-cover group-hover:opacity-80"
+                  unoptimized
+                />
+              </div>
+              <div className="py-8 lg:hidden">
+                <ScrollSyncToc rawMarkdownBody={article.content} />
+              </div>
               <div dangerouslySetInnerHTML={{ __html: articleHtml }} />
             </div>
           </div>
-          <div className="relative hidden pt-24 lg:block">
+          <div className="relative hidden pt-24 lg:col-span-3 lg:block">
             <ScrollSyncToc rawMarkdownBody={article.content} />
           </div>
         </div>
