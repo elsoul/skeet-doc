d77a0fff118e0cd1ece135a2a8151e533a911be5
KishiTheMechanic <s.kishi@elsoul.nl>
2023-06-18 00:31:09 +0200

    add feedback form row

--- src/components/pages/home/SkeetDemoRow.tsx
+++ src/components/pages/home/SkeetDemoRow.tsx
@@ -4,8 +4,53 @@ export default function SkeetDemoRow() {
   const { t } = useTranslation()
   return (
     <>
-      <div className="">
-        <div className="mx-auto max-w-7xl px-6 py-48 sm:py-64 lg:px-8">
+      <div className="relative isolate">
+        <svg
+          className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)] dark:stroke-gray-600"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M.5 200V.5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg
+            x="50%"
+            y={-1}
+            className="overflow-visible fill-gray-50 dark:fill-gray-600"
+          >
+            <path
+              d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth={0}
+            fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
+          />
+        </svg>
+        <div
+          className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 py-64 sm:py-96 lg:px-8">
           <p className="mb-3 text-3xl font-bold leading-8 tracking-tight text-gray-900 dark:text-white sm:text-4xl">
             {t('home:SkeetDemoRow.title1')}
           </p>
