7bd5e707feec3ad2f1304d967aeedbafd9f02e45
KishiTheMechanic <s.kishi@elsoul.nl>
2023-11-26 15:50:42 +0100

    use Inter

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,17 +1,16 @@
 const siteConfig = {
   domain: 'skeet.dev',
   copyright: 'ELSOUL LABO B.V.',
-  sitenameJA:
-    'Skeet - オープンソースのフルスタック TypeScript サーバーレスフレームワーク',
-  sitenameEN: 'Skeet - Open-Source Full-stack TypeScript Serverless Framework',
+  sitenameJA: 'Skeet - TypeScript サーバーレスフレームワーク',
+  sitenameEN: 'Skeet - TypeScript Serverless Framework',
   keywordsJA:
     'Skeet, TypeScript, オープンソース, サーバーレス, フルスタック, フレームワーク, アプリケーション, 開発, ソリューション, Firebase, Firestore, Cloud Functions, GCP, Google Cloud, ゼロメンテナンス, Next.js, Expo, GraphQL, SQL, Prisma',
   keywordsEN:
     'Skeet, TypeScript, Open-Source, Serverless, Full-stack, Framework, Application, Development, Solution, Firebase, Firestore, Cloud Functions, GCP, Google Cloud, Zero Maintenance, Next.js, Expo, GraphQL, SQL, Prisma',
   descriptionJA:
-    'Effortless. Serverless. Skeet は GCP (Google Cloud) と Firebase 上にTypeScript一本でフルスタックアプリを構築できるオープンソースのサーバーレスフレームワークです。',
+    'Just Build It. Skeet はオープンソースのTypeScript製サーバーレスフレームワークです。フルスタックから小規模タスクまで、あらゆるアプリ開発に対応しています。',
   descriptionEN:
-    'Effortless. Serverless. Skeet is an Open-Source Serverless Framework for full-stack TypeScript apps on GCP (Google Cloud) and Firebase.',
+    'Just Build It. Skeet is an open-source TypeScript serverless framework. It supports all kinds of application development, from full-stack to small-scale tasks.',
   twitterAccount: '@SkeetDev',
   instagramAccount: 'elsoul_labo',
   githubAccount: 'elsoul',
